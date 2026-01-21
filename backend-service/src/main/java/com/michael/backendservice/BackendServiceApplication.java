b51de639a37dcc25e123ca2befd2e47f85b45668
Michael Sanchez <mikedansanchez@gmail.com>
2026-01-21 07:35:06 -0500

    M1.2.2: add logging baseline and startup log

--- backend-service/src/main/java/com/michael/backendservice/BackendServiceApplication.java
+++ backend-service/src/main/java/com/michael/backendservice/BackendServiceApplication.java
@@ -1,13 +1,19 @@
 package com.michael.backendservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendServiceApplication {
 
+	private static final Logger log = 
+		LoggerFactory.getLogger(BackendServiceApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackendServiceApplication.class, args);
+		log.info("Backend Service started successfully");
 	}
 
 }

bd1d7b36a7f6ba26f661de3bf3e7ffdbd9ac0a40
Michael Sanchez <mikedansanchez@gmail.com>
2026-01-28 07:27:35 -0500

    Enable Scheduling in Spring Boot in BackendServiceApplication.java

--- backend-service/src/main/java/com/michael/backendservice/BackendServiceApplication.java
+++ backend-service/src/main/java/com/michael/backendservice/BackendServiceApplication.java
@@ -1,19 +1,15 @@
 package com.michael.backendservice;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BackendServiceApplication {
 
-	private static final Logger log = 
-		LoggerFactory.getLogger(BackendServiceApplication.class);
-
-	public static void main(String[] args) {
-		SpringApplication.run(BackendServiceApplication.class, args);
-		log.info("Backend Service started successfully");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendServiceApplication.class, args);
+    }
 
 }

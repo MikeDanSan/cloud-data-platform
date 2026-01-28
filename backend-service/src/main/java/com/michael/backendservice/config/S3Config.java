a38bdcfcb03d0b744d3e19d22b4555a154e533e6
Michael Sanchez <mikedansanchez@gmail.com>
2026-01-28 09:07:34 -0500

    M6.4: Add S3Client Bean in config/S3Config.java

--- backend-service/src/main/java/com/michael/backendservice/config/S3Config.java
+++ backend-service/src/main/java/com/michael/backendservice/config/S3Config.java
@@ -3,7 +3,6 @@ package com.michael.backendservice.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -11,19 +10,20 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class S3Config {
 
+    @Value("${app.aws.region}")
+    private String awsRegion;
+
     @Bean
-    public S3Client s3Client(@Value("${app.aws.region}") String region) {
+    public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.of(awsRegion))
                 .build();
     }
 
     @Bean
-    public S3Presigner s3Presigner(@Value("${app.aws.region}") String region) {
+    public S3Presigner s3Presigner() {
         return S3Presigner.builder()
-                .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.of(awsRegion))
                 .build();
     }
 }
